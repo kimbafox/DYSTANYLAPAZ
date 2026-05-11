9e48712baacf17ef78efe08adcfb6be2c6949851
kimbafox <juegocrisger@gmail.com>
2026-05-10 21:28:57 -0400

    Delete DYSTANYLAPAZ-main directory

--- DYSTANYLAPAZ-main/frontend/js/dev-toggle.js
+++
@@ -1,6 +0,0 @@
-const devBtn = document.getElementById("devFloating");
-const devMessage = document.getElementById("devMessage");
-
-devBtn.addEventListener("click", () => {
-  devMessage.classList.toggle("show");
-});
